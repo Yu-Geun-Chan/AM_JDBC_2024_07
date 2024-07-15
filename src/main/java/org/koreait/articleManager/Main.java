719e7368dc4c28d015bbd2c501c4db4c0f8d47f0
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-15 16:46:45 +0900

    Main 클래스에서 sql오류를 볼 수 있게 처리

--- src/main/java/org/koreait/articleManager/Main.java
+++ src/main/java/org/koreait/articleManager/Main.java
@@ -1,10 +1,18 @@
 package org.koreait.articleManager;
 
+import org.koreait.exception.SQLErrorException;
+
+
 public class Main {
     public static void main(String[] args) {
 
         Container.init();
 
-        new App().run();
+        try {
+            new App().run();
+        } catch (SQLErrorException e) {
+            System.err.println("e.getMessage : " + e.getMessage());
+            e.getOrigin().printStackTrace();
+        }
     }
 }
